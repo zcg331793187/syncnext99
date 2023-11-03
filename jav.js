83d66fae19dfd7100727366c85b9669ace21bfca
Scofield <zhoucaiguang@gmail.com>
2023-11-03 15:10:17 +0800

    fix

--- jav.js
+++ jav.js
@@ -9,6 +9,7 @@ function buildMedias(inputURL) {
             "User-Agent": "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66"
         }
     };
+    console.log(req.url);
     $http.fetch(req).then(res => {
         let datas = [];
         // const _body = res.body.replace(/[\n\t]+/g, '').replace(/>[\s]+</g, '><');
